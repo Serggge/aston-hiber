a84b7156f8b36135b91f28fca719f1d07722b8e4
Serggge <serggge69@yandex.ru>
2025-09-01 05:38:33 +0300

    done

--- src/main/java/ru/serggge/dao/UserRepositoryImpl.java
+++ src/main/java/ru/serggge/dao/UserRepositoryImpl.java
@@ -1,8 +1,11 @@
 package ru.serggge.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.query.Query;
 import ru.serggge.entity.User;
 import java.util.Optional;
 import java.util.Properties;
@@ -17,22 +20,46 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(User user) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        }
+        return user;
     }
 
     @Override
     public Optional<User> findById(Long userId) {
-        return Optional.empty();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query<User> namedQuery = session.createNamedQuery("findById", User.class)
+                                            .setParameter("id", userId);
+            User user = namedQuery.getSingleResultOrNull();
+            transaction.commit();
+            return Optional.ofNullable(user);
+        }
     }
 
     @Override
     public User update(User user) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            user = session.merge(user);
+            transaction.commit();
+        }
+        return user;
     }
 
     @Override
     public void deleteById(Long userId) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query<User> namedQuery = session.createNamedQuery("deleteById", User.class)
+                                            .setParameter("id", userId);
+            namedQuery.executeUpdate();
+            transaction.commit();
+            session.flush();
+        }
     }
 
     private void init() {
@@ -41,6 +68,8 @@ public class UserRepositoryImpl implements UserRepository {
         properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:postgresql://localhost:5432/astonhiber");
         properties.put(Environment.JAKARTA_JDBC_USER, "root");
         properties.put(Environment.JAKARTA_JDBC_PASSWORD, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
 
         sessionFactory = new Configuration()
                 .setProperties(properties)
