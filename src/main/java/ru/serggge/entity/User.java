0435eb8c76b9d8f32c5644a1982deff4ab11ea55
Serggge <serggge69@yandex.ru>
2025-09-18 19:26:07 +0300

    annotation removed

--- src/main/java/ru/serggge/entity/User.java
+++ src/main/java/ru/serggge/entity/User.java
@@ -2,13 +2,11 @@ package ru.serggge.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import ru.serggge.annotations.UnitName;
 import java.io.Serializable;
 import java.time.Instant;
 
 @Entity
 @Table(name = "users")
-@UnitName(name = "user-persistence")
 @Getter
 @ToString
 @NoArgsConstructor
